8c51ed4bd2ac9f9cd2f61518d5136e9e44d0a250
tom <tom@tompc>
2012-06-25 17:30:01 +0800

    fragstudy

--- AndroidStudy4/src/com/androidstudy/AndroidStudy4Activity.java
+++ AndroidStudy4/src/com/androidstudy/AndroidStudy4Activity.java
@@ -1,7 +1,10 @@
 package com.androidstudy;
 
 import android.app.Activity;
+import android.app.FragmentManager;
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
 
 public class AndroidStudy4Activity extends Activity {
     /** Called when the activity is first created. */
@@ -9,5 +12,16 @@ public class AndroidStudy4Activity extends Activity {
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.main);
+        
+        FragmentManager fm = getFragmentManager();  
+        fm.findFragmentById(R.id.fragment1);
     }
+    
+    public void toFrag(View view)
+	{
+		
+    	Intent intent = new Intent();
+    	intent.setClass(this, ItemDetailActivity.class);
+    	startActivity(intent);
+	}
 }
