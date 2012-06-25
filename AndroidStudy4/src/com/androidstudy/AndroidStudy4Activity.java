406b4c6729706faf98bf1829d683994b8cd0556a
tom <tom@tompc>
2012-06-25 15:04:33 +0800

    init

---
+++ AndroidStudy4/src/com/androidstudy/AndroidStudy4Activity.java
@@ -0,0 +1,13 @@
+package com.androidstudy;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+public class AndroidStudy4Activity extends Activity {
+    /** Called when the activity is first created. */
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.main);
+    }
+}
